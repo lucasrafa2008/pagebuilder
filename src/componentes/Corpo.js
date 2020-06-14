c686eda8c0678dab26eecd17d993ff214ff5aa1f
Lucas Dias <lucasrafa2008@gmail.com>
2020-06-14 18:16:14 -0300

    Alterações do dia 14-06-20

---
+++ src/componentes/Corpo.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Corpo(props) {
+    const drop = e =>{
+    e.preventDefault();
+    const card_id = e.dataTransfer.getData('card_id');
+
+    const card = document.getElementById(card_id);
+    card.style.display = 'block';
+
+    e.target.appendChild(card);    
+    
+    }
+    const dragover = e => {
+     e.preventDefault();
+    }
+    return (
+        <div
+          id = {props.id}
+          className={props.className}
+          onDrop={drop}
+          onDragOver={dragover}
+        >
+            {props.children}
+                   
+        </div>
+    )
+}
+
+export default Corpo
---
+++ src/componentes/Graficos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Graficos() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Graficos
+
